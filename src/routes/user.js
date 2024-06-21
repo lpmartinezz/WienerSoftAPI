6c20838642b992d521979c10f9abf70aa9ebcd67
lpmartinezz <alpierre@gmail.com>
2024-06-20 20:24:40 -0500

    [main]: inicio proyecto 20062024

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,6 +5,25 @@ const bcrypt = require("bcryptjs");
 const router = express.Router();
 const salt = 10;
 
+// get all user
+router.get("/users", async (req, res) => {
+  userSchema
+    .find()
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+
+});
+
+// get a user
+router.get("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  userSchema
+    .findById(id)
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+
+});
+
 // create user
 router.post("/users", async (req, res) => {
 
@@ -20,6 +39,27 @@ router.post("/users", async (req, res) => {
       .then((data) => res.json(data))
       .catch((error) => res.json({ message: error }));
   
+});
+
+// update user
+router.put("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  const { userName, firstName, lastName, country, departament, estate, city, address, phone, birthdate, email } = req.body;
+  userSchema
+    .updateOne({_id: id}, { $set: { userName, firstName, lastName, country, departament, estate, city, address, phone, birthdate, email} })
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+
+});
+
+// delete user
+router.delete("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  userSchema
+    .deleteOne({ _id: id })
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+
 });
 
 module.exports = router;
